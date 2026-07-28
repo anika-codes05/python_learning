7c8157bd5049552d030e3cb2e9bd975c29939ebd
anikatyagi05 <anika.25161029@kiet.edu>
2026-07-28 15:03:36 +0530

    commit 2
    commit 2

---
+++ practice_string.py
@@ -0,0 +1,9 @@
+#wap to input user's first name & print its length
+str = input("enter a your first name : ")
+print("length of your name is : ", len(str))
+
+#wap to find the occurance of "$" in a string
+st= input("enter a string:")
+print(st.count("$"))
+
+
---
+++ string.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+str='i wanttobe from coder, i am studying python from apna college'
+str2=str[1:7]
+print(str2)
+#functions of string
+print(str.endswith("ege"))
+print(str.capitalize()) # this function change and make new string do not change the original string
+print(str)#original string 
+print(str.replace("o","X"))
+print(str.replace("python","javascript")) # replace (old,new)
+print(str.find("o")) #return the index of the string character asked to find . gets -1 if not found 
+print(str.count("from"))
+
